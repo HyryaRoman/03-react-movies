e8cdbaa37ed247cd55458fd1b730361d328e8b26
Hyria Roman <hyrya.roman@gmail.com>
2026-06-19 13:16:46 +0300

    api: add debug mode

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -3,6 +3,7 @@ import type { AxiosInstance } from "axios";
 import type { Movie } from "../types/movie.ts";
 
 const TMDB_API_KEY = import.meta.env.TMDB_API_KEY;
+const DEBUG_MODE = import.meta.env.DEBUG_MODE || false;
 
 const tmdb: AxiosInstance = axios.create({
   baseURL: `https://api.themoviedb.org/3/`,
@@ -11,9 +12,16 @@ const tmdb: AxiosInstance = axios.create({
   },
 });
 
-export async function fetchMovies(query: string, page: number = 0): Promise<Movie[]> {
-  const response = await tmdb.get("/search/movie", {
-    params: { query, page },
-  });
-  return response.data.results;
+export async function fetchMovies(
+  query: string,
+  page: number = 0,
+): Promise<Movie[]> {
+  if (!DEBUG_MODE) {
+    const response = await tmdb.get("/search/movie", {
+      params: { query, page },
+    });
+    return response.data.results;
+  } else {
+    return [];
+  }
 }
